bd0f80b1d95af404c54a761732f6d5c368872224
Zach58008 <105465971+Zach58008@users.noreply.github.com>
2024-04-15 19:12:52 -0600

    Update data_energy.R

--- data_energy.R
+++ data_energy.R
@@ -1,4 +1,3 @@
-#
 library(tidyverse)
 library(ggfortify)
 library(corrplot)
@@ -10,14 +9,14 @@ library(readxl)
 set.seed(12345)
 
 # import data
-energy <- read_csv("ENB2012_data.csv")
+energy <- read_xlsx('ENB2012_data.xlsx')
 energy <- as.data.frame(energy)
 
 
 # change col names
-colnames(energy) <- c("Compact", "SurfaceA", "WallA", "RoofA", "twoStories", "Orient", "GlazingA", "GlazingADist", "Heat", "Cool")
+colnames(energy) = c('Compact', 'SurfaceA', 'WallA', 'RoofA', 'twoStories', 'Orient', 'GlazingA', 'GlazingADist', 'Heat', 'Cool')
 
-energy
+
 # remove data we are not using
 energy$Orient <- NULL
 energy$GlazingADist <- NULL
@@ -35,35 +34,38 @@ ggplot(data = energy) +
 ggplot(data = energy) +
   geom_boxplot(aes(x = twoStories, y = Heat))
 ggplot(data = energy) +
-  geom_point(aes(x = Compact, y = Heat, color = twoStories), position = "jitter")
+  geom_point(aes(x = Compact, y = Heat, color = twoStories), position = 'jitter')
 ggplot(data = energy) +
-  geom_point(aes(x = SurfaceA, y = Heat, color = twoStories), position = "jitter")
+  geom_point(aes(x = SurfaceA, y = Heat, color = twoStories), position = 'jitter')
 ggplot(data = energy) +
-  geom_point(aes(x = WallA, y = Heat, color = twoStories), position = "jitter")
+  geom_point(aes(x = WallA, y = Heat, color = twoStories), position = 'jitter')
 ggplot(data = energy) +
-  geom_point(aes(x = RoofA, y = Heat, color = twoStories), position = "jitter")
+  geom_point(aes(x = RoofA, y = Heat, color = twoStories), position = 'jitter')
 
-# it looks like there will be some interaction terms with number of stories and
-    # some of the other variables. Heating and cooling also look to be very
+ggplot(data = energy) +
+  geom_point(aes(x = GlazingA, y = Heat, color = twoStories), position = 'jitter')
+# it looks like there will be some interaction terms with number of stories and 
+    # some of the other variables. Heating and cooling also look to be very 
     # closely correlated
 
 
 # check for multicollinearity between continuous variables
 continuous <- energy %>%
-  select(-twoStories)
-corrplot(cor(continuous), type = "upper")
-round(cor(continuous), 2)
+  select(-twoStories, -RoofA, -SurfaceA)
+corrplot(cor(continuous), type = 'upper')
+round(cor(continuous),2)
 # it looks like there is some multicollinearity between the variables
 
 # create a full linear model for both heating and cooling
-heating_lm <- lm(Heat ~ Compact + SurfaceA + WallA + RoofA + twoStories +
+heating_lm <- lm(Heat ~ Compact + SurfaceA + WallA + RoofA + twoStories + 
                    GlazingA, data = energy)
-cooling_lm <- lm(Cool ~ Compact + SurfaceA + WallA + RoofA + twoStories +
+cooling_lm <- lm(Cool ~ Compact + SurfaceA + WallA + RoofA + twoStories + 
                    GlazingA, data = energy)
 summary(heating_lm)
 summary(cooling_lm)
 avPlots(heating_lm)
 avPlots(cooling_lm)
+#vif(heating_lm)
 # both models removed Roof Area, i think because it is perfectly multicollinear
 # with number of stories
 
@@ -83,34 +85,39 @@ boxplot(cooling_lm$residuals)
 # equality of variance
 
 # Interactions
-anova <- aov(Heat ~ Compact * twoStories + SurfaceA * twoStories + WallA * twoStories
+anova <- aov(Heat ~ Compact*twoStories + SurfaceA*twoStories + WallA*twoStories
              + GlazingA, data = energy)
 summary(anova)
-interaction.plot(x.factor = energy$Compact,
-                 trace.factor = energy$twoStories,
+interaction.plot(x.factor = energy$Compact, 
+                 trace.factor = energy$twoStories, 
                  response = energy$Heat)
-interaction.plot(x.factor = energy$SurfaceA,
-                 trace.factor = energy$twoStories,
+interaction.plot(x.factor = energy$SurfaceA, 
+                 trace.factor = energy$twoStories, 
                  response = energy$Heat)
-interaction.plot(x.factor = energy$WallA,
-                 trace.factor = energy$twoStories,
+interaction.plot(x.factor = energy$WallA, 
+                 trace.factor = energy$twoStories, 
                  response = energy$Heat)
-interaction.plot(x.factor = energy$RoofA,
-                 trace.factor = energy$twoStories,
+interaction.plot(x.factor = energy$RoofA, 
+                 trace.factor = energy$twoStories, 
                  response = energy$Heat)
-interaction.plot(x.factor = energy$GlazingA,
-                 trace.factor = energy$twoStories,
+interaction.plot(x.factor = energy$GlazingA, 
+                 trace.factor = energy$twoStories, 
                  response = energy$Heat)
+
+interaction.plot(x.factor = energy$GlazingA, 
+                 trace.factor = energy$Compact, 
+                 response = energy$Heat)
+
 # looks like number of stories has an interaction with  compact, surface area,
 # and wall area
 
 # fit a new model with the interaction terms
-heat_interax_lm <- lm(Heat ~ Compact + SurfaceA + WallA + twoStories +
-                        GlazingA + WallA * twoStories + Compact * twoStories +
-                        SurfaceA * twoStories, data = energy)
-cool_interax_lm <- lm(Cool ~ Compact + SurfaceA + WallA + twoStories +
-                        GlazingA + WallA * twoStories + Compact * twoStories +
-                        SurfaceA * twoStories, data = energy)
+heat_interax_lm <- lm(Heat ~ Compact + SurfaceA + WallA + twoStories + 
+                        GlazingA + WallA*twoStories + Compact*twoStories + 
+                        SurfaceA*twoStories, data = energy)
+cool_interax_lm <- lm(Cool ~ Compact + SurfaceA + WallA + twoStories + 
+                        GlazingA + WallA*twoStories + Compact*twoStories + 
+                        SurfaceA*twoStories, data = energy)
 summary(heat_interax_lm)
 summary(cool_interax_lm)
 autoplot(heat_interax_lm)
@@ -128,16 +135,16 @@ heat <- energy %>%
 cool <- energy %>%
   select(Compact, SurfaceA, WallA, RoofA, twoStories, GlazingA, Cool)
 heat <- as.data.frame(heat)
-cool <- as.data.fram(cool)
-heat_x <- as.matrix(heat[, 1:6])
+cool <- as.data.frame(cool)
+heat_x <- as.matrix(heat[, 1:6]) 
 heat_y <- heat[, 7]
-cool_x <- as.matrix(cool[, 1:6])
+cool_x <- as.matrix(cool[, 1:6]) 
 cool_y <- cool[, 7]
 ## Ridge
 heat_ridge_cv <- cv.glmnet(x = heat_x,
-                           y = heat_y,
-                           type.measure = "mse",
-                           alpha = 0)
+                          y = heat_y, 
+                          type.measure = "mse", 
+                          alpha = 0)
 autoplot(heat_ridge_cv, label = FALSE) +
   theme_bw() +
   theme(aspect.ratio = 1)
@@ -145,8 +152,8 @@ heat_ridge_cv$lambda.1se
 coef(heat_ridge_cv, s = "lambda.1se")
 
 cool_ridge_cv <- cv.glmnet(x = cool_x,
-                           y = cool_y,
-                           type.measure = "mse",
+                           y = cool_y, 
+                           type.measure = "mse", 
                            alpha = 0)
 autoplot(cool_ridge_cv, label = FALSE) +
   theme_bw() +
@@ -155,8 +162,8 @@ cool_ridge_cv$lambda.1se
 coef(cool_ridge_cv, s = "lambda.1se")
 ## LASSO
 heat_lasso_cv <- cv.glmnet(x = heat_x,
-                           y = heat_y,
-                           type.measure = "mse",
+                           y = heat_y, 
+                           type.measure = "mse", 
                            alpha = 1)
 autoplot(heat_lasso_cv, label = FALSE) +
   theme_bw() +
@@ -166,8 +173,8 @@ coef(heat_lasso_cv, s = "lambda.1se")
 
 
 cool_lasso_cv <- cv.glmnet(x = cool_x,
-                           y = cool_y,
-                           type.measure = "mse",
+                           y = cool_y, 
+                           type.measure = "mse", 
                            alpha = 1)
 autoplot(cool_lasso_cv, label = FALSE) +
   theme_bw() +
@@ -176,9 +183,9 @@ cool_lasso_cv$lambda.1se
 coef(cool_lasso_cv, s = "lambda.1se")
 ## Elastic Net
 heat_net_cv <- cv.glmnet(x = heat_x,
-                         y = heat_y,
-                         type.measure = "mse",
-                         alpha = 0.5)
+                           y = heat_y, 
+                           type.measure = "mse", 
+                           alpha = 0.5)
 autoplot(heat_net_cv, label = FALSE) +
   theme_bw() +
   theme(aspect.ratio = 1)
@@ -187,8 +194,8 @@ coef(heat_net_cv, s = "lambda.1se")
 
 
 cool_net_cv <- cv.glmnet(x = cool_x,
-                         y = cool_y,
-                         type.measure = "mse",
+                         y = cool_y, 
+                         type.measure = "mse", 
                          alpha = 0.5)
 autoplot(cool_net_cv, label = FALSE) +
   theme_bw() +
@@ -196,84 +203,157 @@ autoplot(cool_net_cv, label = FALSE) +
 cool_net_cv$lambda.1se
 coef(cool_net_cv, s = "lambda.1se")
 ## Forward
-base_mod <- lm(Heat ~ 1, data = heat)
-full_mod <- lm(Heat ~ ., data = heat)
-base_mod_cool <- lm(Cool ~ 1, data = cool)
+base_mod <- lm(Heat ~ 1, data = heat) 
+full_mod <- lm(Heat ~ ., data = heat) 
+base_mod_cool <- lm(Cool ~ 1, data = cool) 
 full_mod_cool <- lm(Cool ~ ., data = cool)
 forw_BIC <- step(base_mod,
-                 direction = "forward",
-                 scope = list(lower = base_mod, upper = full_mod),
+                 direction = "forward", 
+                 scope=list(lower= base_mod, upper= full_mod),
                  k = log(nrow(heat)))
 summary(forw_BIC)
 
 
 forw_BIC_cool <- step(base_mod_cool,
-                      direction = "forward",
-                      scope=list(lower = base_mod_cool, upper = full_mod_cool),
-                      k = log(nrow(cool)))
+                 direction = "forward", 
+                 scope=list(lower= base_mod_cool, upper= full_mod_cool),
+                 k = log(nrow(cool)))
 summary(forw_BIC_cool)
 ## Backward
 back_BIC <- step(full_mod,
                  direction = "backward", 
-                 scope = list(lower = base_mod, upper = full_mod),
+                 scope=list(lower= base_mod, upper= full_mod),
                  k = log(nrow(heat)))
 summary(back_BIC)
 
 
 back_BIC_cool <- step(full_mod_cool,
-                      direction = "backward",
-                      scope = list(lower = base_mod_cool, upper = full_mod_cool),
-                      k = log(nrow(cool)))
+                 direction = "backward", 
+                 scope=list(lower= base_mod_cool, upper= full_mod_cool),
+                 k = log(nrow(cool)))
 summary(back_BIC_cool)
 ## Sequential
 full_seq_BIC <- step(full_mod,
-                     direction = "both",
-                     scope = list(lower = base_mod, upper = full_mod),
-                     k = log(nrow(heat)))
+                     direction = "both", 
+                     scope=list(lower= base_mod, upper= full_mod),
+                     k=log(nrow(heat)))
 summary(full_seq_BIC)
 
 
 full_seq_BIC_cool <- step(full_mod_cool,
-                          direction = "both",
-                          scope = list(lower = base_mod_cool, upper = full_mod_cool),
-                          k = log(nrow(cool)))
+                     direction = "both", 
+                     scope=list(lower= base_mod_cool, upper= full_mod_cool),
+                     k=log(nrow(cool)))
 summary(full_seq_BIC_cool)
 
-# all the models for both explanatory variables agree to remove roof area and
+# all the models for both explanatory variables agree to remove roof area and 
     # keep compact, wall area, stories, and glazing area. LASSO and Net say
     # remove surface area, forward and backward say keep it
 
-# in the interest of model simplicity / interpretablilty, I say that we drop surface area -Spencer
 
-cool_reduced_model <- lm(Cool ~ Compact + WallA + twoStories + GlazingA, data = energy)
-heat_reduced_model <- lm(Heat ~ Compact + WallA + twoStories + GlazingA, data = energy)
 
-autoplot(heat_reduced_model)
-autoplot(heat_reduced_model, which = 4)
+invTranPlot(log(Heat) ~ Compact , data = heat, lambda = c(-1,0,1,2))
+invTranPlot(log(Heat) ~ WallA , data = heat, lambda = c(-1,0,1,2))
+invTranPlot(log(Heat) ~ GlazingA, data = heat, lambda = c(-1,0,1,2))
 
-autoplot(cool_reduced_model)
-autoplot(cool_reduced_model, which = 4)
 
-shapiro.test(heat_reduced_model$residuals)
-hist(heat_reduced_model$residuals)
-boxplot(heat_reduced_model$residuals)
+invTranPlot(log(Cool) ~ Compact , data = cool, lambda = c(-1,0,1,2))
+invTranPlot(log(Cool) ~ WallA , data = cool, lambda = c(-1,0,1,2))
 
-shapiro.test(cool_reduced_model$residuals)
-hist(cool_reduced_model$residuals)
-boxplot(cool_reduced_model$residuals)
+bc = boxCox(heating_lm)
+lambda.opt = bc$x[which.max(bc$y)]
+lambda.opt
+heat_trans_lm <- lm(log(Heat) ~ 1/(WallA**2) + twoStories + 
+                      GlazingA + twoStories*GlazingA, data = heat)
 
-# We fixed the multicollinearity but we're still going to need to transform the data; normality is still violated
+cool_trans_lm <- lm(log(Cool) ~ log(Compact) + log(WallA) + twoStories + 
+                      GlazingA + twoStories*GlazingA, data = cool)
 
-boxCox(heat_reduced_model)
-boxCox(cool_reduced_model)
+#######
 
-# lambda = 0 (log transformation) of the response might be a good fit for both of these models
 
-cool_reduced_log_model <- lm(Cool ~ Compact + log(WallA) + twoStories + log(GlazingA), data = energy)
-heat_reduced_log_model <- lm(Heat ~ Compact + log(WallA) + twoStories + log(GlazingA), data = energy)
+
+
+cool_trans_lm_small <- lm(log(Cool) ~ log(WallA) + twoStories + 
+                      GlazingA + twoStories*GlazingA, data = cool) # best model so far
+
+
+
+######
+
+
+
+anova(cool_trans_lm, cool_trans_lm_small)
+anova(cool_trans_lm_small, cool_trans_lm)
+
+
+
+confint(cool_trans_lm)
+
+dat <- data.frame(Compact = .75, WallA = 345, twoStories = 1, GlazingA = 0.1)
+dat$twoStories <- as.factor(dat$twoStories)
+
+predict(cool_trans_lm, newdata = dat, interval = 'confidence', level = .9)
+
+
+predict(wind_lm, newdata = data.frame(RSpd = 8), interval = 'prediction',level=.9)
+
+summary(cool_trans_lm)
+autoplot(cool_trans_lm)
+shapiro.test(cool_trans_lm$residuals)
+hist(cool_trans_lm$residuals)
+boxplot(cool_trans_lm$residuals)
+
+summary(heat_trans_lm)
+autoplot(heat_trans_lm)
+shapiro.test(heat_trans_lm$residuals)
+hist(heat_trans_lm$residuals)
+boxplot(heat_trans_lm$residuals)
+
+ggplot(data = heat)+
+  geom_point(aes(x = log(Compact), y = log(Heat), color = twoStories))+
+  geom_line(mapping = aes(x = log(Compact),
+                          y = predict(heat_trans_lm), 
+                          color = twoStories))
+ggplot(data = heat)+
+  geom_point(aes(x = 1/(WallA**2), y = log(Heat), color = twoStories))+
+  geom_line(mapping = aes(x = 1/(WallA**2),
+                          y = predict(heat_trans_lm), 
+                          color = twoStories))
+ggplot(data = heat)+
+  geom_point(aes(x = GlazingA, y = log(Heat), color = twoStories))+
+  geom_line(mapping = aes(x = GlazingA,
+                          y = predict(heat_trans_lm), 
+                          color = twoStories))
+
+
+
+
+
+
+
+cool_reduced_log_model <- lm(Cool ~ sqrt(WallA) + twoStories + sqrt(GlazingA) + twoStories*WallA, data = energy)
+cool_reduced <- lm(sqrt(Cool) ~ sqrt(WallA) + twoStories + sqrt(GlazingA), data = energy)
+heat_reduced_log_model <- lm(sqrt(Heat) ~ Compact + sqrt(WallA) + twoStories + sqrt(GlazingA), data = energy)
+heat$residuals <- heat_reduced_log_model$residuals
+cor(energy)
+
+ggplot(data = heat) +
+  geom_point(mapping = aes(x = Compact, y = residuals)) +
+  theme(aspect.ratio = 1)
+
+ggplot(data = heat) +
+  geom_point(mapping = aes(x = sqrt(WallA), y = residuals)) +
+  theme(aspect.ratio = 1)
+ggplot(data = heat) +
+  geom_point(mapping = aes(x = sqrt(GlazingA), y = residuals)) +
+  theme(aspect.ratio = 1)
+
+
 
 autoplot(heat_reduced_log_model)
 autoplot(heat_reduced_log_model, which = 4)
+autoplot(cool_reduced)
 
 autoplot(cool_reduced_log_model)
 autoplot(cool_reduced_log_model, which = 4)
